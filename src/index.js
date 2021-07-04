d003196ac9884c9c550357ff812c3782a4a7137d
Alina Melnyk <alinkaaa345@gmail.com>
2021-07-04 17:25:00 +0300

    добавляет шаблон меню

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import './sass/main.scss';
+import menu from './menu.json';
+import eventsTemplate from './templates/menu-items.hbs';
+
+// import './sass/main.scss';
+
+const mainMenu = document.querySelector('.js-menu');
+mainMenu.innerHTML = eventsTemplate(menu);
+
