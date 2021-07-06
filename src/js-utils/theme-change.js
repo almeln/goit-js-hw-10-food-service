4188855c84dda7c4ec76513364cddd3ea9d13e3e
Alina Melnyk <alinkaaa345@gmail.com>
2021-07-06 21:49:35 +0300

    убирает дублирование кода

--- src/js-utils/theme-change.js
+++ src/js-utils/theme-change.js
@@ -1,36 +1,68 @@
 import { refs } from './refs';
 import { Theme } from './theme';
 
+// export function onCheckboxChange(event) {
+//     const checkedCheckbox = event.currentTarget.checked;
+
+//     if(checkedCheckbox) {
+//         onChecked();
+//     } else {
+//         onUsual();
+//     }
+// }
+
 export function onCheckboxChange(event) {
     const checkedCheckbox = event.currentTarget.checked;
 
-    if(checkedCheckbox === true) {
-        onChecked();
+    if(checkedCheckbox) {
+        themeSwitcher(Theme.LIGHT, Theme.DARK);
     } else {
-        onUsual();
+        themeSwitcher(Theme.DARK, Theme.LIGHT);
     }
 }
 
-export function onChecked() {
-    refs.body.classList.remove(Theme.LIGHT);
-    refs.body.classList.add(Theme.DARK);
-    localStorage.setItem('theme', Theme.DARK);
-    refs.switcher.checked = true;
+function themeSwitcher(themeToRemove, themeToAdd) {
+    refs.body.classList.remove(themeToRemove);
+    refs.body.classList.add(themeToAdd);
+    localStorage.setItem('theme', themeToAdd);
+
+    if (themeToAdd === Theme.LIGHT) {
+        refs.switcher.checked = false;
+    } else {
+        refs.switcher.checked = true;
+    }
 }
 
-export function onUsual() {
-    refs.body.classList.remove(Theme.DARK);
-    refs.body.classList.add(Theme.LIGHT);
-    localStorage.setItem('theme', Theme.LIGHT);
-    refs.switcher.checked = false;
-}
+// function onChecked() {
+//     refs.body.classList.remove(Theme.LIGHT);
+//     refs.body.classList.add(Theme.DARK);
+//     localStorage.setItem('theme', Theme.DARK);
+//     refs.switcher.checked = true;
+// }
+
+// function onUsual() {
+//     refs.body.classList.remove(Theme.DARK);
+//     refs.body.classList.add(Theme.LIGHT);
+//     localStorage.setItem('theme', Theme.LIGHT);
+//     refs.switcher.checked = false;
+// }
+
+// export function onCurrentTheme() {
+//     const currentTheme = localStorage.getItem('theme');
+
+//     if(currentTheme === Theme.LIGHT || currentTheme === null) {
+//         onUsual();
+//     } else {
+//         onChecked();
+//     }
+// }
 
 export function onCurrentTheme() {
     const currentTheme = localStorage.getItem('theme');
 
     if(currentTheme === Theme.LIGHT || currentTheme === null) {
-        onUsual();
+        themeSwitcher(Theme.DARK, Theme.LIGHT);;
     } else {
-        onChecked();
+        themeSwitcher(Theme.LIGHT, Theme.DARK);
     }
 }
